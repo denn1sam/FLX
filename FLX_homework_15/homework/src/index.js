a9f00bbecda5c48063103d062c2b60493b36da05
Den Samotos <38587580+denn1sam@users.noreply.github.com>
2019-03-22 16:24:10 +0200

    Add files via upload

--- FLX_homework_15/homework/src/index.js
+++ FLX_homework_15/homework/src/index.js
@@ -2,13 +2,20 @@
 const messages = {
   smallSalary: 'Cannot set smaller salary than employee has now',
   noCompany: 'Employee don`t work in any company',
-  dontEmployeeObj: 'Please try to add Employee instance'
+  hasCompany: 'Employee work in company now!',
+  dontEmployeeObj: 'Please try to add Employee instance',
+  wrongCompanyArgument: 'The company accepts only object with name, owner, maxCompanySize in its argument'
 }
 const one = 1;
 const two = 2;
 const thousand = 1000;
 
 function Company(company) {
+
+  if (typeof company !== 'object' || arguments.length > two) {
+    return console.log(messages.wrongCompanyArgument);
+  }
+
   this.companyName = company.name;
   this.owner = company.owner;
   this.maxCompanySize = company.maxCompanySize;
@@ -16,29 +23,27 @@ function Company(company) {
   let _currentEmployees = [];
   let _logs = [];
 
-  _logs.push(`${this.companyName} was created in ${new Date().toLocaleString()}, by ${this.owner}`);
+  function getDate() {
+    return new Date();
+  }
+  
+  _logs.push(`${this.companyName} was created in ${getDate()}, by ${this.owner}`);
 
   this.addNewEmployee = function(employee) {
     if (employee instanceof Employee) {
       if (_currentEmployees.length < this.maxCompanySize) {
         employee.hire(this.companyName);
         _currentEmployees.push(employee);
-        _logs.push(`${employee.name} starts working at ${this.companyName} in ${new Date().toLocaleString()}`);
+        _logs.push(`${employee.name} starts working at ${this.companyName} in ${getDate()}`);
       } else {
         let currentIndex = 0;
         let minEmpSalary = _currentEmployees[currentIndex].salary;
-        let biggestWorkTime = _currentEmployees[currentIndex].getWorkTimeInSeconds();
 
         for (let i = 1; i < _currentEmployees.length; i++) {
           if (minEmpSalary > _currentEmployees[i].salary) {
             currentIndex = i;
             minEmpSalary = _currentEmployees[i].salary;
-          } else if (minEmpSalary === _currentEmployees[i].salary) {
-            if (biggestWorkTime < _currentEmployees[i].getWorkTimeInSeconds()) {
-              currentIndex = i;
-              minEmpSalary = _currentEmployees[i].salary;
-            }
-          }
+          } 
         }
         this.removeEmployee(currentIndex);
         this.addNewEmployee(employee);
@@ -49,8 +54,8 @@ function Company(company) {
   };
 
   this.removeEmployee = function(id) {
-    if (!isNaN(parseFloat(id)) && isFinite(id)) {
-      _logs.push(`${_currentEmployees[id].name} ends working in ${this.companyName} in ${new Date().toLocaleString()}`);
+    if (!isNaN(parseInt(id)) && isFinite(id)) {
+      _logs.push(`${_currentEmployees[id].name} ends working in ${this.companyName} in ${getDate()}`);
       _currentEmployees[id].fire();
       _currentEmployees.splice(id, one);
     }
@@ -80,7 +85,7 @@ function Company(company) {
 
   this.getFormattedListOfEmployees = function() {
     return _currentEmployees.map(function(emp) {
-      return `${emp.name} - works in ${this.companyName}`;
+      return `${emp.name} - works in ${emp.company} ${emp.getWorkTimeInSeconds()} seconds`;
     });
   };
 
@@ -122,14 +127,18 @@ function Employee(employee) {
   };
 
   this.hire = function(currentCompanyName) {
-    _startDate = getDate();
-    this.company = currentCompanyName;
-    _logs.push(`${this.name} is hired to ${this.company} in ${new Date().toLocaleString()}`);
+    if (this.company) {
+      return console.log(messages.hasCompany);
+    } else {
+      _startDate = getDate();
+      this.company = currentCompanyName;
+      _logs.push(`${this.name} is hired to ${this.company} in ${getDate()}`);
+    }
   };
 
   this.fire = function() {
     _finalDate = getDate();
-    _logs.push(`${this.name} is fired from ${this.company} in ${new Date().toLocaleString()}`);
+    _logs.push(`${this.name} is fired from ${this.company} in ${getDate()}`);
     this.company ? delete this.company : console.log(messages.noCompany);
   };
 
@@ -139,58 +148,58 @@ function Employee(employee) {
 }
 
 //code example
-let artem = new Employee({name: 'Artem', age: 15, salary: 1000, primarySkill: 'UX'});
-let vova = new Employee({name: 'Vova', age: 16, salary: 2000, primarySkill: 'BE'});
-let vasyl = new Employee({name: 'Vasyl', age: 25, salary: 1000, primarySkill: 'FE'});
-let ivan = new Employee({name: 'Ivan', age: 35, salary: 5000, primarySkill: 'FE'});
-let orest = new Employee({name: 'Orest', age: 29, salary: 300, primarySkill: 'AT'});
-let anton = new Employee({name: 'Anton', age: 19, salary: 500, primarySkill: 'Manager'});
+// let artem = new Employee({name: 'Artem', age: 15, salary: 1000, primarySkill: 'UX'});
+// let vova = new Employee({name: 'Vova', age: 16, salary: 2000, primarySkill: 'BE'});
+// let vasyl = new Employee({name: 'Vasyl', age: 25, salary: 1000, primarySkill: 'FE'});
+// let ivan = new Employee({name: 'Ivan', age: 35, salary: 5000, primarySkill: 'FE'});
+// let orest = new Employee({name: 'Orest', age: 29, salary: 300, primarySkill: 'AT'});
+// let anton = new Employee({name: 'Anton', age: 19, salary: 500, primarySkill: 'Manager'});
 
-let epam = new Company({name: 'Epam', owner: 'Arkadii', maxCompanySize: 5});
-epam.addNewEmployee(artem);
-epam.addNewEmployee(vova);
-epam.addNewEmployee(vasyl);
-epam.addNewEmployee(ivan);
-epam.addNewEmployee(orest);
-epam.addNewEmployee(anton);
+// let epam = new Company({name: 'Epam', owner: 'Arkadii', maxCompanySize: 5});
+// epam.addNewEmployee(artem);
+// epam.addNewEmployee(vova);
+// epam.addNewEmployee(vasyl);
+// epam.addNewEmployee(ivan);
+// epam.addNewEmployee(orest);
+// epam.addNewEmployee(anton);
 
-console.log(epam.getHistory());
+// console.log(epam.getHistory());
 
-/*
-"Epam was created in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time)"
-"Artem starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
-"Vova starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
-"Vasyl starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
-"Ivan starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
-"Orest starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
-"Orest ends working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
-"Anton starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
-*/
-epam.removeEmployee(2);
+// /*
+// "Epam was created in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time)"
+// "Artem starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
+// "Vova starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
+// "Vasyl starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
+// "Ivan starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
+// "Orest starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
+// "Orest ends working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
+// "Anton starts working at Epam in Tue Mar 12 2019 07:41:50 GMT+0200 (FLE Standard Time);"
+// */
+// epam.removeEmployee(2);
 
 
-console.log(vasyl.getHistory());
+// console.log(vasyl.getHistory());
 
-/*
-"Vasyl is hired to Epam in Tue Mar 12 2019 07:45:55 GMT+0200 (FLE Standard Time)"
-"Vasyl is fired from Epam in Tue Mar 12 2019 07:45:55 GMT+0200 (FLE Standard Time)"
-*/
+// /*
+// "Vasyl is hired to Epam in Tue Mar 12 2019 07:45:55 GMT+0200 (FLE Standard Time)"
+// "Vasyl is fired from Epam in Tue Mar 12 2019 07:45:55 GMT+0200 (FLE Standard Time)"
+// */
 
-console.log(epam.getAvarageSalary()); // -> 2125
-console.log(epam.getAvarageAge()); // -> 21.25
+// console.log(epam.getAvarageSalary()); // -> 2125
+// console.log(epam.getAvarageAge()); // -> 21.25
 
-epam.addNewEmployee(5,6,9,5); // -> Please try to add Employee instance
+// epam.addNewEmployee(5,6,9,5); // -> Please try to add Employee instance
 
-setTimeout(() => {
-   epam.removeEmployee(1);
-   console.log(artem.getWorkTimeInSeconds()); // -> 5.5744444444444445
-}, 5000);
+// setTimeout(() => {
+//    epam.removeEmployee(1);
+//    console.log(artem.getWorkTimeInSeconds()); // -> 5.5744444444444445
+// }, 5000);
 
-vova.setSalary(900);
-vova.setSalary(2200);
-console.log(vova.getHistory());
-/*
-"Vova is hired to Epam in Tue Mar 12 2019 08:08:48 GMT+0200 (FLE Standard Time)"
-"try to change salary from 2000 to 900"
-"change salary from 2000 to 2200"
-*/
+// vova.setSalary(900);
+// vova.setSalary(2200);
+// console.log(vova.getHistory());
+// /*
+// "Vova is hired to Epam in Tue Mar 12 2019 08:08:48 GMT+0200 (FLE Standard Time)"
+// "try to change salary from 2000 to 900"
+// "change salary from 2000 to 2200"
+// */
